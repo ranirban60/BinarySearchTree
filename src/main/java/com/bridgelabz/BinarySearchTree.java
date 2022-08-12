854536196f2a15ddc501511827cdaae437f22841
ranirban60@gmail.com <ranirban60@gmail.com>
2022-08-12 21:23:26 +0530

    Added: Ability to search in Binary Tree

--- src/main/java/com/bridgelabz/BinarySearchTree.java
+++ src/main/java/com/bridgelabz/BinarySearchTree.java
@@ -8,7 +8,7 @@ package com.bridgelabz;
     }
     // All methods are nonstatic in the class so create object for this class in main method
     public class BinarySearchTree {
-        //insert method is used here to insert root and value
+        //insert() method is used here to insert root and value
         public Node insert(Node root, int val) {
             if (root == null) {
                 return createNewNode(val);
@@ -35,7 +35,7 @@ package com.bridgelabz;
             System.out.print(root.data +"->");
             print(root.right);
         }
-        //Size method For checking all nodes are added or Not
+        //Size method For checking all nodes are added or not
         public int size(Node node) {
             /*
              *  if condition is true then node is empty if condition is false then
@@ -45,6 +45,24 @@ package com.bridgelabz;
                 return 0;
             else
                 return(size(node.left) + 1 + size(node.right));
+        }
+        //nodePresent method this is parameterized method.root and value are passed
+        public boolean nodePresent(Node root, int val) {
+            if (root == null) {
+                return false;
+            }
+            Boolean isPresent = false;
+            while (root != null) {
+                if (val < root.data) {
+                    root = root.left;
+                } else if (val > root.data) {
+                    root = root.right;
+                } else {
+                    isPresent = true;
+                    break;
+                }
+            }
+            return isPresent;
         }
     }
 
--- src/main/java/com/bridgelabz/BinarySearchTreeMain.java
+++ src/main/java/com/bridgelabz/BinarySearchTreeMain.java
@@ -1,5 +1,5 @@
 /*
-Ability to create a Binary Tree
+Ability to search 63 in the Binary Tree
  */
 
 package com.bridgelabz;
@@ -10,7 +10,7 @@ public class BinarySearchTreeMain {
 
         BinarySearchTree bst = new BinarySearchTree();
         Node root = null;
-        // inserting the value in root
+        // Inserting the value in root
         root = bst.insert(root, 56); // 56 is adding in root
         root = bst.insert(root, 30);
         root = bst.insert(root, 70);
@@ -26,6 +26,16 @@ public class BinarySearchTreeMain {
         root = bst.insert(root, 67);
 
         bst.print(root);
+        // If condition is true then First print the output is boolean type because this is boolean method
+        System.out.println(bst.nodePresent(root, 63));
+        /*
+         * if else statement is used if condition is true then print Element 63 is present in binary tree
+         * if condition false then print Element 63 is not present in binary tree.
+         */
+        if(root != null)
+            System.out.println("\nElement 63 is present in the Binary tree");
+        else
+            System.out.println("\nElement 63 is not present in the Binary tree");
 
     }
 }
